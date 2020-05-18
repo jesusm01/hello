fb8ab426a0a1718c6de694a0e01d22617493033e
Jesus <jesusmoral011096@gmail.com>
2020-05-18 03:40:44 +0200

    problema 3

--- codigo.js
+++ codigo.js
@@ -6,25 +6,10 @@ function voltear (parentesis){
     return parentesisv;
 }
 
-function buscar (palabra){
-    valores= [0,0];
-    if(palabra.indexOf("(") != -1){
-        valores[0] = palabra.indexOf("(");
-        valores[1] = palabra.lastIndexOf(")");
-        var nueva = palabra.slice(valores[0]+1,valores[1]);
-        var volt = buscar (nueva);
-        valores[0] = palabra.indexOf("(");
-        valores[1] = palabra.lastIndexOf(")");
-        var nuevainicio = palabra.slice(0,valores[0]);
-        var nuevafinal = palabra.slice(valores[1]+1,palabra.length);
-        var final = nuevainicio+volt+nuevafinal;
-        return final;
-    }
-    else{
-        var volteado = voltear(palabra);
-        return volteado;
-    }
+var palabra = "amor a roma";
+var palabrav = voltear (palabra);
+if(palabra == palabrav){
+    console.log("Si es palindromo");
 }
-
-var palabra = "foo(bar)";
-console.log(buscar(palabra));
+else
+    console.log("No es palindromo");
